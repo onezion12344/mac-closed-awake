5c380d517fb94be190f217f79b35c2f7a1d7d3c9
onezion12344 <onezion12344@users.noreply.github.com>
2026-07-31 13:53:36 +0800

    chore: bump version to 1.1.0 with lid-close fix

--- cask/mac-closed-awake.rb
+++ cask/mac-closed-awake.rb
@@ -1,6 +1,6 @@
 cask "mac-closed-awake" do
-  version "1.0.0"
-  sha256 "679d800c032cc0f1a28d53c3783f291ad610a49772c398b6164dddc4a01ca762"
+  version "1.1.0"
+  sha256 "21ec375ad2a570c31602305a4c1afdf99e6894231e95f337c775c8ebf1bc5e67"
 
   url "https://github.com/onezion12344/mac-closed-awake/releases/download/v#{version}/MacClosedAwake-v#{version}.dmg"
   name "MacClosedAwake"
